176f6b3755ffce9e702b03ef788537d0b5a0ce4a
ManujChaturvedi <aeceaysim&13>
2021-01-04 12:51:36 +0530

    Merge Sort Added.

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class BubbleSort {
 
-    HelperFunctions hf = new HelperFunctions();
+    HelperFunctions helperFunctions = new HelperFunctions();
 
     public void sort(int[] arr,int n){
         for (int unsortedIndex=n-1;unsortedIndex>0;unsortedIndex--){
             for (int i=0;i<unsortedIndex;i++){
                 if(arr[i]>arr[i+1]){
-                    hf.swap(arr,i,i+1);
+                    helperFunctions.swap(arr,i,i+1);
                 }
             }
         }
